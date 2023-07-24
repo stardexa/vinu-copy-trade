a68e1a63887307f5cc7fd75565ee3aa30d4a1f8f
stardexa <skworkk@gmail.com>
2023-07-24 23:21:32 +0300

    chart js,react-chartjs-2 entegre

---
+++ src/@vinu/components/charts/DonutChart/index.tsx
@@ -0,0 +1,71 @@
+import styles from './AreaChart.module.scss'
+import React from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Filler,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Filler,
+    Legend
+);
+
+
+
+const AreaChart = ({title,label,data1,data2}) => {
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom' as const,
+            },
+            title: {
+                display: true,
+                text: title,
+            },
+        },
+    };
+
+    const labels = label;
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                fill: true,
+                label: 'Dataset 1',
+                data: data1,
+                borderColor: 'rgb(255, 149, 92)',
+                backgroundColor: 'rgba(255, 149, 92, 0.2)',
+            },
+            {
+                fill: true,
+                label: 'Dataset 2',
+                data: data2,
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgba(53, 162, 235, 0.2)',
+            },
+        ],
+    };
+
+    return (
+        <>
+            <Line options={options} data={data} />
+        </>
+    );
+ }
+export default AreaChart
--- src/@vinu/components/profile/Statistics/index.tsx
+++ src/@vinu/components/profile/Statistics/index.tsx
@@ -1,59 +1,18 @@
-import styles from './Statistics.module.scss'
-import Image from "next/image";
-
-import React from 'react';
-import { Line } from 'react-chartjs-2';
-
-const Statistics = ()=> {
-    const data = {
-        labels: ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs'],
-        datasets: [
-            {
-                label: 'Satışlar',
-                data: [12, 19, 3, 5, 2],
-                borderColor: 'rgba(75, 192, 192, 1)',
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                fill: true,
-            },
-        ],
-    };
-
-    const options = {
-        scales: {
-            y: {
-                beginAtZero: true,
-            },
-        },
-    };
+import AreaChart from "../../charts/AreaChart";
 
 
-    // new Chart(ctx, {
-    //     type: 'bar',
-    //     data: {
-    //         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    //         datasets: [{
-    //             label: '# of Votes',
-    //             data: [12, 19, 3, 5, 2, 3],
-    //             borderWidth: 1
-    //         }]
-    //     },
-    //     options: {
-    //         scales: {
-    //             y: {
-    //                 beginAtZero: true
-    //             }
-    //         }
-    //     }
-    // });
-    //
+const Statistics = () => {
     return (
-        <>
-            Statistics
-            <div>
-                <h2>Satışlar Grafiği</h2>
-                <Line data={data} options={options} />
-            </div>
-        </>
-    )
-}
-export default Statistics
+        <AreaChart
+            title={'chart test'}
+            label={['January', 'February', 'March', 'April', 'May', 'June', 'July']}
+            data1={[31,42,35,34,41,27,37]}
+            data2={[30,50,45,34,21,39,51]}
+        />
+    );
+};
+
+export default Statistics;
+
+
+
